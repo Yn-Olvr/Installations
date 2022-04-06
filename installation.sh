d42ed1779954d35d650fc37f7567c190385a0523
Yn-Olvr <yannd38@gmail.com>
2022-04-06 18:40:21 -0400

    New version of installation.sh

--- installation.sh
+++ installation.sh
@@ -1,31 +1,35 @@
-echo "Here is the inventory of our system"
-
-echo "======================================================"
-
-#read ans
-
-#touch $ans
+#!/bin/bash
+#Author: Yann-Olvier Mbella
+#date: 06 Avril 2022
+#Description: We are creating a script to inventory any system
 
 for OS in $(cat /etc/os-release | grep PRETTY_NAME= | awk -F '"' '{print$2}' | awk -F ' ' '{print$1}')
 
 do
 	        mkdir $OS
 		        cd $OS
+
+			touch info
+
+			echo "Our kernel version is" >> info
+			#sleep 3
+			uname -r >> info
+			echo " ====================================" >> info
+			echo "The memory is ou system is" >> info
+			#sleep 3
+			free -m >> info
+			echo " ====================================" >> info
+			echo "The version of our system(bits) is" >> info
+			arch >> info
+			echo " ====================================" >> info
+			echo "Here is the numer of processors on this system" >> info
+			#sleep 3
+			nproc >> info
+
 		done
 
-		touch info
+		echo "A file with the name of your linux flavor has been created with a file named "info"
 
-		echo "Our kernel version is" >> info
-		uname -r >> info
-		echo " ====================================" >> info
-		echo "The memory is ou system is" >> info
-		free -m >> info
-		echo " ====================================" >> info
-		echo "The version of our system(bits) is" >> info
-		arch >> info
-		echo " ====================================" >> info
-		echo "Here is the numer of processors on this system" >> info
-		nproc >> info
+		You can cat that file to see the inventory of your system
 
-
-cat info
+		Thank you !"
